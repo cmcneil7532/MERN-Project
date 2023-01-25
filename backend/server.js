22bbb53ce3ede373d2b7041804c7cae60beef126
Christian McNeil <christianmcneil@Christians-MBP.fios-router.home>
2023-01-24 19:21:22 -0500

    Intialize Mongoose

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,25 @@ const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
 const { handleError } = require("./middleware/errorMiddleware");
+const colors = require("colors");
+const uri = process.env.MONGO_URI;
+//------------Mongoose----------
 
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", true);
+
+mongoose.connect(uri, { useNewUrlParser: true });
+
+const db = mongoose.connection;
+db.once("open", () => {
+  console.log("Database Connected");
+});
+
+db.on("error", (err) => {
+  console.error("conncetion error", err);
+});
+//-------------Mongoose--------------
 const app = express();
-
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
